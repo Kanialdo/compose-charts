932ce37f3ff317719c8736af38501cd46bd9735a
Krystian Kaniowski <kanialdo@gmail.com>
2022-02-15 22:29:01 +0100

    Add methods to draw text on canvas

---
+++ lib/src/desktopMain/kotlin/pl/krystiankaniowski/composecharts/internal/ChartsText.kt
@@ -0,0 +1,26 @@
+package pl.krystiankaniowski.composecharts.internal
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.nativeCanvas
+import org.jetbrains.skia.Font
+import org.jetbrains.skia.Paint
+import org.jetbrains.skia.TextLine
+
+internal actual fun DrawScope.drawText(
+    text: String,
+    x: Float,
+    y: Float,
+    color: Color,
+    size: Float
+) {
+    val textLine = TextLine.Companion.make(text, Font(typeface = null, size = size))
+    drawContext.canvas.nativeCanvas.drawTextLine(
+        line = textLine,
+        x = x - (textLine.width) / 2,
+        y = y,
+        paint = Paint().apply {
+            this.color = 0x88888888.toInt()
+        }
+    )
+}
