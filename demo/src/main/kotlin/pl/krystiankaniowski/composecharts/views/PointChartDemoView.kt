428f64c9f4f43a2a8e117cca4056a046c740741f
Krystian Kaniowski <kanialdo@gmail.com>
2022-02-20 23:26:35 +0100

    detekt fixes

--- demo/src/main/kotlin/pl/krystiankaniowski/composecharts/views/PointChartDemoView.kt
+++ demo/src/main/kotlin/pl/krystiankaniowski/composecharts/views/PointChartDemoView.kt
@@ -22,7 +22,8 @@ fun PointChartDemo() {
                     )
                 ),
                 PointChartData.Points(
-                    label = "Series B", values = listOf(
+                    label = "Series B",
+                    values = listOf(
                         PointChartData.Point(5f, 5f),
                         PointChartData.Point(4f, 4f),
                         PointChartData.Point(3f, 3f),
@@ -31,7 +32,8 @@ fun PointChartDemo() {
                     )
                 ),
                 PointChartData.Points(
-                    label = "Series C", values = listOf(
+                    label = "Series C",
+                    values = listOf(
                         PointChartData.Point(5f, 0f),
                         PointChartData.Point(4f, 0f),
                         PointChartData.Point(3f, 0f),
