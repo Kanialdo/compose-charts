bf0b4e29d395fa7e820fe21198138bc3fc2117d4
Krystian Kaniowski <kanialdo@gmail.com>
2022-04-25 23:13:59 +0200

    Drop old algorithm

--- lib/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/internal/YLines.kt
+++ lib/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/internal/YLines.kt
@@ -1,33 +1,9 @@
 package pl.krystiankaniowski.composecharts.internal
 
-import kotlin.math.*
-
-internal fun calculateYHelperLines(yMin: Float, yMax: Float): List<Float> {
-    val diff = (yMax - yMin)
-    val log = log10(diff)
-    val rounded = log.toInt()
-    val base = (diff * 10f.pow(-rounded)).toInt()
-
-    val step = when (base) {
-        4, 8 -> 4
-        0, 1, 2, 5, 10 -> 5
-        3, 6 -> 6
-        7 -> 7
-        9 -> 9
-        else -> -1
-    }
-
-    return List(step) { i ->
-        val temp = (yMin + (diff / step) * (i + 1))
-        if (temp == 0f) {
-            0f
-        } else {
-            val tempLog = log10(temp).toInt()
-            val value = (temp * 10f.pow(-tempLog)).roundToInt() * 10f.pow(tempLog)
-            value
-        }
-    }
-}
+import kotlin.math.ceil
+import kotlin.math.log10
+import kotlin.math.pow
+import kotlin.math.round
 
 internal fun calculateHelperValues(from: Float, to: Float, desiredCount: Int, withBottomOffset: Boolean = true, withTopOffset: Boolean = true): List<Float> {
 
--- lib/src/commonTest/kotlin/pl/krystiankaniowski/composecharts/internal/YLinesTest.kt
+++ lib/src/commonTest/kotlin/pl/krystiankaniowski/composecharts/internal/YLinesTest.kt
@@ -1,37 +1,8 @@
 import pl.krystiankaniowski.composecharts.internal.calculateHelperValues
-import pl.krystiankaniowski.composecharts.internal.calculateYHelperLines
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 internal class YLinesTest {
-
-    @Test
-    fun testPositive() {
-        assertEquals(listOf(2f, 4f, 6f, 8f, 10f), calculateYHelperLines(0f, 10f))
-        assertEquals(listOf(20f, 40f, 60f, 80f, 100f), calculateYHelperLines(0f, 100f))
-        assertEquals(listOf(2f, 4f, 6f, 8f), calculateYHelperLines(0f, 8f))
-    }
-
-    @Ignore
-    @Test
-    fun testSmallNumbers() {
-        assertEquals(listOf(0.2f, 0.4f, 0.6f, 0.8f, 1f), calculateYHelperLines(0f, 0.1f))
-    }
-
-    @Ignore
-    @Test
-    fun testNegative() {
-        assertEquals(listOf(-10f, -8f, -6f, -4f, -2f), calculateYHelperLines(-10f, 0f))
-        assertEquals(listOf(-0.6f, -0.2f, 0.2f, 0.6f, 1f), calculateYHelperLines(-1f, 1f))
-    }
-
-    @Test
-    fun testIrregular() {
-        assertEquals(listOf(2f, 3f, 4f, 5f, 6f), calculateYHelperLines(1f, 6.2f))
-    }
-
-    // -----
 
     @Test
     fun test1() {
