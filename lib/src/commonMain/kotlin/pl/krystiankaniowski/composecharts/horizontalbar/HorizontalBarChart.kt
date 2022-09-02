35a06bb465b19e013d2adb160a8ef580aa824d6f
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-02 23:28:27 +0200

    Add horizontal bar chart (#66)

---
+++ lib/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/horizontalbar/HorizontalBarChart.kt
@@ -0,0 +1,213 @@
+package pl.krystiankaniowski.composecharts.horizontalbar
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import pl.krystiankaniowski.composecharts.ChartsTheme
+import pl.krystiankaniowski.composecharts.internal.ChartChoreographer
+import pl.krystiankaniowski.composecharts.internal.PointMapper
+import pl.krystiankaniowski.composecharts.legend.LegendEntry
+import pl.krystiankaniowski.composecharts.legend.LegendFlow
+import pl.krystiankaniowski.composecharts.legend.LegendPosition
+
+data class HorizontalBarChartData(val dataSets: List<DataSet>) {
+
+    constructor(vararg dataSets: DataSet) : this(dataSets.toList())
+
+    data class DataSet(
+        val label: String,
+        val color: Color,
+        val values: List<Float>,
+    )
+
+    init {
+        check(dataSets.first().values.size.let { size -> dataSets.all { it.values.size == size } }) {
+            "All bars have to contains same amount of entries"
+        }
+    }
+
+    internal val minValue = dataSets.minOf { it.values.minOf { it } }
+    internal val maxValue = dataSets.maxOf { it.values.maxOf { it } }
+
+    internal val size: Int get() = dataSets.first().values.size
+}
+
+enum class HorizontalBarChartStyle {
+    STANDARD,
+    STACKED,
+    PROPORTION,
+}
+
+@Composable
+fun HorizontalBarChart(
+    modifier: Modifier = Modifier,
+    data: HorizontalBarChartData,
+    style: HorizontalBarChartStyle = HorizontalBarChartStyle.STANDARD,
+    title: (@Composable () -> Unit)? = null,
+    legendPosition: LegendPosition = LegendPosition.Bottom,
+) {
+    ChartChoreographer(
+        modifier = modifier,
+        title = title,
+        legend = { HorizontalBarLegend(data) },
+        legendPosition = legendPosition,
+    ) {
+        Canvas(Modifier.fillMaxSize()) {
+
+            val w = 0.25f
+            val offset = 1f
+
+            val contentArea = Rect(
+                top = 0f, bottom = size.height - 0,
+                left = 0f, right = size.width,
+            )
+            val yAxisArea = Rect(
+                top = contentArea.top, bottom = contentArea.bottom,
+                left = 0f, right = contentArea.left,
+            )
+
+            when (style) {
+
+                HorizontalBarChartStyle.STANDARD -> {
+
+                    val mapper = PointMapper(
+                        xSrcMin = 0f,
+                        xSrcMax = data.maxValue,
+                        xDstMin = contentArea.left,
+                        xDstMax = contentArea.right,
+                        ySrcMin = 0f - offset,
+                        ySrcMax = data.size - 1 + offset,
+                        yDstMin = contentArea.top,
+                        yDstMax = contentArea.bottom,
+                    )
+
+                    // yAxis.draw(this, contentArea, yAxisArea, mapper, mapper.ySrcMin, mapper.ySrcMax)
+
+                    val barHeight = 2 * w * mapper.yScale / data.dataSets.size
+                    data.dataSets.forEachIndexed { series, value ->
+                        value.values.forEachIndexed { pos, v ->
+                            drawRect(
+                                color = value.color,
+                                topLeft = Offset(
+                                    x = mapper.x(0),
+                                    y = mapper.y(pos + w) + series * barHeight,
+                                ),
+                                size = Size(
+                                    width = v * mapper.yScale,
+                                    height = barHeight,
+                                )
+                            )
+                        }
+                    }
+                }
+
+                HorizontalBarChartStyle.STACKED -> {
+
+                    val series = data.dataSets.size
+                    val values = data.size
+                    val maxValues = FloatArray(values) { index -> data.dataSets.map { it.values[index] }.sum() }
+                    val maxOfValues = maxValues.maxOf { it }
+
+                    val mapper = PointMapper(
+                        xSrcMin = 0f,
+                        xSrcMax = maxOfValues,
+                        xDstMin = contentArea.left,
+                        xDstMax = contentArea.right,
+                        ySrcMin = 0f - offset,
+                        ySrcMax = data.size - 1 + offset,
+                        yDstMin = contentArea.top,
+                        yDstMax = contentArea.bottom,
+                    )
+
+                    val barHeight = 2 * w * mapper.yScale
+
+                    // yAxis.draw(this, contentArea, yAxisArea, mapper, mapper.ySrcMin, mapper.ySrcMax)
+
+                    for (i in (values - 1) downTo 0) {
+                        var counter = maxValues[i]
+                        for (j in (series - 1) downTo 0) {
+                            drawRect(
+                                color = data.dataSets[j].color,
+                                topLeft = Offset(
+                                    x = mapper.x(0),
+                                    y = mapper.y(i + w),
+                                ),
+                                size = Size(
+                                    width = counter * mapper.yScale,
+                                    height = barHeight,
+                                )
+                            )
+                            counter -= data.dataSets[j].values[i]
+                        }
+                    }
+                }
+
+                HorizontalBarChartStyle.PROPORTION -> {
+
+                    val series = data.dataSets.size
+                    val values = data.size
+                    val maxValues = FloatArray(values) { index -> data.dataSets.map { it.values[index] }.sum() }
+
+                    val mapper = PointMapper(
+                        xSrcMin = 0f,
+                        xSrcMax = 1f,
+                        xDstMin = contentArea.left,
+                        xDstMax = contentArea.right,
+                        ySrcMin = 0f - offset,
+                        ySrcMax = data.size - 1 + offset,
+                        yDstMin = contentArea.top,
+                        yDstMax = contentArea.bottom,
+                    )
+
+                    // yAxis.draw(this, contentArea, yAxisArea, mapper, mapper.ySrcMin, mapper.ySrcMax)
+
+                    val barHeight = 2 * w * mapper.yScale
+
+                    for (i in (values - 1) downTo 0) {
+                        var counter = maxValues[i]
+                        for (j in (series - 1) downTo 0) {
+                            drawRect(
+                                color = data.dataSets[j].color,
+                                topLeft = Offset(
+                                    x = mapper.x(0),
+                                    y = mapper.y(i + w),
+                                ),
+                                size = Size(
+                                    width = (counter / maxValues[i]) * mapper.xScale,
+                                    height = barHeight,
+                                )
+                            )
+                            counter -= data.dataSets[j].values[i]
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun HorizontalBarLegend(
+    data: HorizontalBarChartData,
+) {
+    Box(modifier = Modifier.border(width = 1.dp, color = ChartsTheme.legendColor)) {
+        LegendFlow(
+            modifier = Modifier.padding(16.dp),
+            data = data.dataSets.mapIndexed { index, item ->
+                LegendEntry(
+                    item.label,
+                    item.color,
+                )
+            }
+        )
+    }
+}
---
+++ lib/src/desktopMain/kotlin/pl/krystiankaniowski/composecharts/horizontalbar/HorizontalBarChartPreview.kt
@@ -0,0 +1,44 @@
+package pl.krystiankaniowski.composecharts.horizontalbar
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import pl.krystiankaniowski.composecharts.autoColor
+
+private val data = HorizontalBarChartData(
+    dataSets = listOf(
+        HorizontalBarChartData.DataSet(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
+        HorizontalBarChartData.DataSet(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
+        HorizontalBarChartData.DataSet(label = "Series C", color = autoColor(2), values = listOf(0f, 1f, 2f, 1f, 0f)),
+    )
+)
+
+@Preview
+@Composable
+fun HorizontalBarChartStandardPreview() {
+    HorizontalBarChart(
+        data = data,
+        style = HorizontalBarChartStyle.STANDARD,
+        title = { Text("Horizontal bar chart standard") },
+    )
+}
+
+@Preview
+@Composable
+fun HorizontalBarChartCombinePreview() {
+    HorizontalBarChart(
+        data = data,
+        style = HorizontalBarChartStyle.STACKED,
+        title = { Text("Horizontal bar chart stacked") },
+    )
+}
+
+@Preview
+@Composable
+fun HorizontalBarChartProportionPreview() {
+    HorizontalBarChart(
+        data = data,
+        style = HorizontalBarChartStyle.PROPORTION,
+        title = { Text("Horizontal bar chart proportion") },
+    )
+}
