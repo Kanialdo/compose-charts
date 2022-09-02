b587228cc4ac863acfd270c9385944677444f778
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-02 21:45:13 +0200

    wip

--- lib/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/radar/RadarChart.kt
+++ lib/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/radar/RadarChart.kt
@@ -3,6 +3,7 @@ package pl.krystiankaniowski.composecharts.radar
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,9 +14,14 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pl.krystiankaniowski.composecharts.ChartsTheme
 import pl.krystiankaniowski.composecharts.internal.ChartChoreographer
+import pl.krystiankaniowski.composecharts.internal.TextAnchorX
+import pl.krystiankaniowski.composecharts.internal.TextAnchorY
+import pl.krystiankaniowski.composecharts.internal.drawText
 import pl.krystiankaniowski.composecharts.legend.LegendEntry
 import pl.krystiankaniowski.composecharts.legend.LegendFlow
 import pl.krystiankaniowski.composecharts.legend.LegendPosition
@@ -35,15 +41,29 @@ data class RadarChartData(
     )
 }
 
+data class RadarChartStyle(
+    val lineColor: Color = Color.Gray,
+    val lineWidth: Dp = 1.dp,
+    val labelColor: Color = Color.Gray,
+    val valueColor: Color = Color.Gray,
+)
+
+@Composable
+fun radarChartStyle() = RadarChartStyle(
+    lineColor = if (MaterialTheme.colors.isLight) Color.LightGray else Color.DarkGray,
+    lineWidth = 1.dp,
+    labelColor = Color.Gray,
+    valueColor = Color.Gray,
+)
+
 @Composable
 fun RadarChart(
     modifier: Modifier = Modifier,
     title: (@Composable () -> Unit)? = null,
     data: RadarChartData,
+    style: RadarChartStyle = radarChartStyle(),
     legendPosition: LegendPosition = LegendPosition.Bottom,
 ) {
-    val line: Color = Color.Gray
-
     val maxValue = data.entries.maxOf { it.values.maxOf { it } }
 
     ChartChoreographer(
@@ -70,14 +90,24 @@ fun RadarChart(
             ) {
                 data.labels.forEachIndexed { index, it ->
                     val angle = 2 * PI / data.labels.size * index + PI
+                    val point = Offset(
+                        x = r * sin(angle).toFloat() + chartArea.center.x,
+                        y = r * cos(angle).toFloat() + chartArea.center.y
+                    )
                     drawLine(
-                        color = line,
+                        color = style.lineColor,
                         start = chartArea.center,
-                        end = Offset(
-                            x = r * sin(angle).toFloat() + chartArea.center.x,
-                            y = r * cos(angle).toFloat() + chartArea.center.y
-                        ),
-                        strokeWidth = 1f
+                        end = point,
+                        strokeWidth = style.lineWidth.toPx()
+                    )
+                    drawText(
+                        text = it,
+                        x = point.x,
+                        y = point.y,
+                        color = style.labelColor,
+                        size = 16.sp.toPx(),
+                        anchorX = TextAnchorX.Left,
+                        anchorY = TextAnchorY.Center,
                     )
                 }
 
@@ -86,10 +116,25 @@ fun RadarChart(
                     (0..data.labels.size).forEachIndexed { index, it ->
                         val angle = 2 * PI / data.labels.size * index + PI
                         if (index == 0) {
-                            path.moveTo(
+                            val point = Offset(
                                 x = r * (value / maxValue) * sin(angle).toFloat() + chartArea.center.x,
                                 y = r * (value / maxValue) * cos(angle).toFloat() + chartArea.center.y
                             )
+                            path.moveTo(
+                                x = point.x,
+                                y = point.y
+                            )
+
+                            drawText(
+                                text = " $value",
+                                x = point.x,
+                                y = point.y,
+                                color = style.valueColor,
+                                size = 14.sp.toPx(),
+                                anchorX = TextAnchorX.Left,
+                                anchorY = TextAnchorY.Center,
+                            )
+
                         } else {
                             path.lineTo(
                                 x = r * (value / maxValue) * sin(angle).toFloat() + chartArea.center.x,
@@ -98,7 +143,7 @@ fun RadarChart(
                         }
                     }
                     path.close()
-                    drawPath(path = path, color = Color.LightGray, style = Stroke(width = 1f))
+                    drawPath(path = path, color = style.lineColor, style = Stroke(width = style.lineWidth.toPx()))
                 }
 
                 data.entries.forEach { entry ->
