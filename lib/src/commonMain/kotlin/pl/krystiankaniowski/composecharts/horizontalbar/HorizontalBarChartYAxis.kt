a1f91acbc97e1aeb0e10e1b2df20375db78a6a04
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-05 23:03:59 +0200

    wip

---
+++ lib/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/horizontalbar/HorizontalBarChartYAxis.kt
@@ -0,0 +1,100 @@
+package pl.krystiankaniowski.composecharts.horizontalbar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+import pl.krystiankaniowski.composecharts.ChartsTheme
+import pl.krystiankaniowski.composecharts.internal.TextAnchorX
+import pl.krystiankaniowski.composecharts.internal.TextAnchorY
+import pl.krystiankaniowski.composecharts.internal.YMapper
+import pl.krystiankaniowski.composecharts.internal.drawText
+
+object HorizontalBarChartYAxis {
+
+    interface Drawer {
+        fun requiredWidth(): Float
+        fun draw(
+            drawScope: DrawScope,
+            chartScope: Rect,
+            yAxisScope: Rect,
+            yMapper: YMapper,
+            data: HorizontalBarChartData,
+        )
+    }
+
+    @Composable
+    fun None(): Drawer = object : Drawer {
+        override fun requiredWidth() = 0f
+        override fun draw(
+            drawScope: DrawScope,
+            chartScope: Rect,
+            yAxisScope: Rect,
+            yMapper: YMapper,
+            data: HorizontalBarChartData,
+        ) {
+        }
+    }
+
+    @Composable
+    fun Auto(
+        textSize: TextUnit = 24.sp,
+        color: Color = ChartsTheme.axisColor,
+    ): Drawer = object : Drawer {
+
+        override fun requiredWidth() = 80f
+
+        override fun draw(
+            drawScope: DrawScope,
+            chartScope: Rect,
+            yAxisScope: Rect,
+            yMapper: YMapper,
+            data: HorizontalBarChartData,
+        ) {
+            drawScope.drawLine(
+                color = color,
+                start = Offset(yAxisScope.right, yAxisScope.top),
+                end = Offset(yAxisScope.right, yAxisScope.bottom),
+            )
+
+            data.labels.forEachIndexed { index, label ->
+                val y = yMapper.y(index)
+                if (data.labels.size <= 10 || (index % (data.labels.size / 10) == 0)) {
+                    drawScope.drawText(
+                        text = label + " ",
+                        x = yAxisScope.right,
+                        y = yAxisScope.top + y,
+                        anchorX = TextAnchorX.Right,
+                        anchorY = TextAnchorY.Center,
+                        color = color,
+                        size = textSize.value,
+                    )
+                }
+            }
+
+//            for (threshold in thresholds) {
+//                val y = yMapper.y(threshold)
+//                drawScope.drawLine(
+//                    color = color,
+//                    start = Offset(x = chartScope.left, y = y),
+//                    end = Offset(x = chartScope.right, y = y),
+//                    pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 20f)),
+//                )
+//                drawScope.drawText(
+//                    text = label(threshold),
+//                    x = requiredWidth() - 10f,
+//                    y = y,
+//                    anchorX = TextAnchorX.Right,
+//                    anchorY = TextAnchorY.Center,
+//                    color = color,
+//                    size = textSize.value,
+//                )
+//            }
+
+
+        }
+    }
+}
--- lib/src/desktopMain/kotlin/pl/krystiankaniowski/composecharts/horizontalbar/HorizontalBarChartPreview.kt
+++ lib/src/desktopMain/kotlin/pl/krystiankaniowski/composecharts/horizontalbar/HorizontalBarChartPreview.kt
@@ -6,6 +6,9 @@ import androidx.compose.runtime.Composable
 import pl.krystiankaniowski.composecharts.autoColor
 
 private val data = HorizontalBarChartData(
+    labels = listOf(
+        "C1", "C2", "C3", "C4", "C5",
+    ),
     dataSets = listOf(
         HorizontalBarChartData.DataSet(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
         HorizontalBarChartData.DataSet(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
