ff8ec2a50f642ef37f7dae22d0aae970903e8dea
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-14 17:53:32 +0200

    Change style

--- demo/src/main/kotlin/pl/krystiankaniowski/composecharts/DemoTheme.kt
+++ demo/src/main/kotlin/pl/krystiankaniowski/composecharts/DemoTheme.kt
@@ -1,6 +1,29 @@
 package pl.krystiankaniowski.composecharts
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-//fun
-//class DemoTheme {
-//}
+private val LightColors = lightColors(
+    primary = Color(0xFFE6194B),
+    onPrimary = Color.White,
+)
+
+private val DarkColors = darkColors(
+    primary = Color(0xFF9a1636),
+    onPrimary = Color.White,
+)
+
+@Composable
+fun DemoTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkColors else LightColors,
+        content = content,
+    )
+}
