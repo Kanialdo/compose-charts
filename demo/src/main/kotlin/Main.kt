cd527797dd32ed8e4eef0dd173a93a21ed536f96
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-14 22:16:50 +0200

    Add icon to Demo (#102)

--- demo/src/main/kotlin/Main.kt
+++ demo/src/main/kotlin/Main.kt
@@ -1,20 +1,17 @@
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import pl.krystiankaniowski.composecharts.DemoTheme
 import pl.krystiankaniowski.composecharts.DemoView
 
 fun main() = application {
+
+    val icon = painterResource("icon.png")
+
     Window(
         onCloseRequest = ::exitApplication,
+        icon = icon,
         title = "Compose charts",
-    ) {
-        MaterialTheme(
-            colors = if (isSystemInDarkTheme()) darkColors() else lightColors()
-        ) {
-            DemoView()
-        }
-    }
+        content = { DemoTheme { DemoView() } },
+    )
 }
