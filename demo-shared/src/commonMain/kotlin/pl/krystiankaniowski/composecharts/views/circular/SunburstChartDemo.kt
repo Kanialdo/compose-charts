44e3ab66eea0f0a8ddab735342e6da1939479c27
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-21 23:02:22 +0200

    Fix sunburst naming

--- demo-shared/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/views/circular/SunburstChartDemo.kt
+++ demo-shared/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/views/circular/SunburstChartDemo.kt
@@ -3,43 +3,43 @@ package pl.krystiankaniowski.composecharts.views.circular
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import pl.krystiankaniowski.composecharts.autoColor
-import pl.krystiankaniowski.composecharts.circular.SunbrustChart
-import pl.krystiankaniowski.composecharts.circular.SunbrustChartData
+import pl.krystiankaniowski.composecharts.circular.SunburstChart
+import pl.krystiankaniowski.composecharts.circular.SunburstChartData
 
 @Suppress("MagicNumber")
 @Composable
-fun SunbrustChartDemo() {
-    SunbrustChart(
-        data = SunbrustChartData(
+fun SunburstChartDemo() {
+    SunburstChart(
+        data = SunburstChartData(
             slices = listOf(
-                SunbrustChartData.Slice(
+                SunburstChartData.Slice(
                     label = "Series A",
                     color = autoColor(0),
                     value = 3f,
                     subSlices = listOf(
-                        SunbrustChartData.Slice(
+                        SunburstChartData.Slice(
                             label = "Series A1",
                             color = autoColor(1),
                             value = 1f,
                             subSlices = listOf(
-                                SunbrustChartData.Slice(label = "Series A1.1", color = autoColor(2), value = 0.5f),
+                                SunburstChartData.Slice(label = "Series A1.1", color = autoColor(2), value = 0.5f),
                             ),
                         ),
-                        SunbrustChartData.Slice(
+                        SunburstChartData.Slice(
                             label = "Series A2",
                             color = autoColor(3),
                             value = 2f,
                             subSlices = listOf(
-                                SunbrustChartData.Slice(label = "Series A1.1", color = autoColor(4), value = 0.5f),
-                                SunbrustChartData.Slice(label = "Series A1.2", color = autoColor(5), value = 0.25f),
+                                SunburstChartData.Slice(label = "Series A1.1", color = autoColor(4), value = 0.5f),
+                                SunburstChartData.Slice(label = "Series A1.2", color = autoColor(5), value = 0.25f),
                             ),
                         ),
                     )
                 ),
-                SunbrustChartData.Slice(label = "Series B", color = autoColor(6), value = 2f),
-                SunbrustChartData.Slice(label = "Series C", color = autoColor(7), value = 1f),
+                SunburstChartData.Slice(label = "Series B", color = autoColor(6), value = 2f),
+                SunburstChartData.Slice(label = "Series C", color = autoColor(7), value = 1f),
             ),
         ),
-        title = { Text("Sunbrust chart") },
+        title = { Text("Sunburst chart") },
     )
 }
