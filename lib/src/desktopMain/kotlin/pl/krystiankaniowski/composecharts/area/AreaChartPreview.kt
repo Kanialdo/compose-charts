4fa46a5c2ccc3bf5afcae97a2e44c271e7d83b95
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-21 22:52:15 +0200

    wip

--- lib/src/desktopMain/kotlin/pl/krystiankaniowski/composecharts/area/AreaChartPreview.kt
+++ lib/src/desktopMain/kotlin/pl/krystiankaniowski/composecharts/area/AreaChartPreview.kt
@@ -1,4 +1,4 @@
-package pl.krystiankaniowski.composecharts.line
+package pl.krystiankaniowski.composecharts.area
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Text
@@ -7,164 +7,164 @@ import androidx.compose.ui.graphics.Color
 import pl.krystiankaniowski.composecharts.autoColor
 import kotlin.random.Random
 
-private val data = LineChartData(
+private val data = AreaChartData(
     lines = listOf(
-        LineChartData.Line(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
-        LineChartData.Line(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
-        LineChartData.Line(label = "Series C", color = autoColor(2), values = listOf(0f, 1f, 2f, 1f, 0f)),
+        AreaChartData.Area(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
+        AreaChartData.Area(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
+        AreaChartData.Area(label = "Series C", color = autoColor(2), values = listOf(0f, 1f, 2f, 1f, 0f)),
     )
 )
 
 @Preview
 @Composable
-fun LineChartPreview() {
-    LineChart(
+fun AreaChartPreview() {
+    AreaChart(
         data = data,
-        title = { Text("Line chart") },
+        title = { Text("Area chart") },
     )
 }
 
 @Preview
 @Composable
-fun LineChartNotRegularPreview() {
-    LineChart(
-        data = LineChartData(
+fun AreaChartNotRegularPreview() {
+    AreaChart(
+        data = AreaChartData(
             lines = listOf(
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series A",
                     color = autoColor(0),
                     values = listOf(1.2f, 8.35f, 16.6f, 54.987f, 62.99f),
                 ),
             )
         ),
-        title = { Text("Line chart") },
+        title = { Text("Area chart") },
     )
 }
 
 @Preview
 @Composable
-fun LineChartCustomLabelsPreview() {
-    LineChart(
-        data = LineChartData(
+fun AreaChartCustomLabelsPreview() {
+    AreaChart(
+        data = AreaChartData(
             lines = listOf(
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series A",
                     color = autoColor(0),
                     values = listOf(1f, 2f, 3f, 4f, 5f),
                 ),
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series B",
                     color = autoColor(1),
                     values = listOf(2f, 3f, 5f, 4f, 1f),
                 ),
             )
         ),
-        title = { Text("Line chart") },
-        xAxis = LineChartXAxis.Auto(label = { listOf("A", "B", "C", "D", "E")[it] }),
+        title = { Text("Area chart") },
+        xAxis = AreaChartXAxis.Auto(label = { listOf("A", "B", "C", "D", "E")[it] }),
     )
 }
 
 
 @Preview
 @Composable
-fun LineChartCustomStylePreview() {
-    LineChart(
-        data = LineChartData(
+fun AreaChartCustomStylePreview() {
+    AreaChart(
+        data = AreaChartData(
             lines = listOf(
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Custom color",
                     values = listOf(5f, 4f, 3f, 2f, 1f),
                     color = Color.Black,
                 ),
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Custom line style",
                     values = listOf(1f, 1f, 1f, 1f, 1f),
-                    lineStyle = LineChartStyle.LineStyle(width = 5f),
+                    lineStyle = AreaChartStyle.AreaStyle(width = 5f),
                     color = autoColor(1),
                 ),
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Custom point style",
                     values = listOf(0f, 1f, 2f, 1f, 0f),
-                    pointStyle = LineChartStyle.PointStyle.Filled(size = 5f),
+                    pointStyle = AreaChartStyle.PointStyle.Filled(size = 5f),
                     color = autoColor(2),
                 ),
             )
         ),
-        style = LineChartStyle(
-            lineStyle = LineChartStyle.LineStyle(width = 1f),
-            pointStyle = LineChartStyle.PointStyle.None,
+        style = AreaChartStyle(
+            lineStyle = AreaChartStyle.AreaStyle(width = 1f),
+            pointStyle = AreaChartStyle.PointStyle.None,
         ),
-        title = { Text("Line chart") },
+        title = { Text("Area chart") },
     )
 }
 
 @Preview
 @Composable
-fun LineChartComplexPreview() {
+fun AreaChartComplexPreview() {
     val random = Random(0)
     val points = List(50) { random.nextFloat() * 5 }
-    LineChart(
-        data = LineChartData(
+    AreaChart(
+        data = AreaChartData(
             lines = listOf(
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series A",
                     color = autoColor(0),
                     values = points,
                 )
             )
         ),
-        title = { Text("Line chart") },
+        title = { Text("Area chart") },
     )
 }
 
 @Preview
 @Composable
-fun LineStackedChartComplexPreview() {
+fun AreaStackedChartComplexPreview() {
     val random = Random(0)
     val points1 = List(50) { random.nextFloat() * 5 }
     val points2 = List(50) { random.nextFloat() * 5 }
-    LineChart(
-        data = LineChartData(
+    AreaChart(
+        data = AreaChartData(
             lines = listOf(
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series A",
                     color = autoColor(0),
                     values = points1,
                 ),
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series B",
                     color = autoColor(1),
                     values = points2,
                 ),
             )
         ),
-        mode = LineChartMode.STACKED,
-        title = { Text("Line stacked chart") },
+        mode = AreaChartMode.STACKED,
+        title = { Text("Area stacked chart") },
     )
 }
 
 @Preview
 @Composable
-fun LineProportionalChartComplexPreview() {
+fun AreaProportionalChartComplexPreview() {
     val random = Random(0)
     val points1 = List(50) { random.nextFloat() * 5 + 1 }
     val points2 = List(50) { random.nextFloat() * 5 + 1 }
     val points3 = List(50) { random.nextFloat() * 5 + 1 }
-    LineChart(
-        data = LineChartData(
+    AreaChart(
+        data = AreaChartData(
             lines = listOf(
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series A", color = autoColor(0), values = points1,
                 ),
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series B", color = autoColor(1), values = points2,
                 ),
-                LineChartData.Line(
+                AreaChartData.Area(
                     label = "Series C", color = autoColor(2), values = points3,
                 ),
             )
         ),
-        mode = LineChartMode.PROPORTIONAL,
-        title = { Text("Line proportional chart") },
+        mode = AreaChartMode.PROPORTIONAL,
+        title = { Text("Area proportional chart") },
     )
 }
---
+++ lib/src/desktopMain/kotlin/pl/krystiankaniowski/composecharts/line/areaChartPreview.kt
@@ -0,0 +1,99 @@
+package pl.krystiankaniowski.composecharts.line
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import pl.krystiankaniowski.composecharts.autoColor
+import kotlin.random.Random
+
+private val data = LineChartData(
+    lines = listOf(
+        LineChartData.Line(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
+        LineChartData.Line(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
+        LineChartData.Line(label = "Series C", color = autoColor(2), values = listOf(0f, 1f, 2f, 1f, 0f)),
+    )
+)
+
+@Preview
+@Composable
+fun LineChartPreview() {
+    LineChart(
+        data = data,
+        title = { Text("Line chart") },
+    )
+}
+
+@Preview
+@Composable
+fun LineChartNotRegularPreview() {
+    LineChart(
+        data = LineChartData(
+            lines = listOf(
+                LineChartData.Line(
+                    label = "Series A",
+                    color = autoColor(0),
+                    values = listOf(1.2f, 8.35f, 16.6f, 54.987f, 62.99f),
+                ),
+            )
+        ),
+        title = { Text("Line chart") },
+    )
+}
+
+@Preview
+@Composable
+fun LineChartCustomLabelsPreview() {
+    LineChart(
+        data = LineChartData(
+            lines = listOf(
+                LineChartData.Line(
+                    label = "Series A",
+                    color = autoColor(0),
+                    values = listOf(1f, 2f, 3f, 4f, 5f),
+                ),
+                LineChartData.Line(
+                    label = "Series B",
+                    color = autoColor(1),
+                    values = listOf(2f, 3f, 5f, 4f, 1f),
+                ),
+            )
+        ),
+        title = { Text("Line chart") },
+        xAxis = LineChartXAxis.Auto(label = { listOf("A", "B", "C", "D", "E")[it] }),
+    )
+}
+
+
+@Preview
+@Composable
+fun LineChartCustomStylePreview() {
+    LineChart(
+        data = LineChartData(
+            lines = listOf(
+                LineChartData.Line(
+                    label = "Custom color",
+                    values = listOf(5f, 4f, 3f, 2f, 1f),
+                    color = Color.Black,
+                ),
+                LineChartData.Line(
+                    label = "Custom line style",
+                    values = listOf(1f, 1f, 1f, 1f, 1f),
+                    lineStyle = LineChartStyle.LineStyle(width = 5f),
+                    color = autoColor(1),
+                ),
+                LineChartData.Line(
+                    label = "Custom point style",
+                    values = listOf(0f, 1f, 2f, 1f, 0f),
+                    pointStyle = LineChartStyle.PointStyle.Filled(size = 5f),
+                    color = autoColor(2),
+                ),
+            )
+        ),
+        style = LineChartStyle(
+            lineStyle = LineChartStyle.LineStyle(width = 1f),
+            pointStyle = LineChartStyle.PointStyle.None,
+        ),
+        title = { Text("Line chart") },
+    )
+}
