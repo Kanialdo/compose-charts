589442fec5ffdb56b66bc51e05dbec26f0c46c26
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-21 22:37:20 +0200

    Add ability to manage bar and column demo charts

--- demo-shared/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/views/ColumnChartDemoView.kt
+++ demo-shared/src/commonMain/kotlin/pl/krystiankaniowski/composecharts/views/ColumnChartDemoView.kt
@@ -1,65 +1,107 @@
 package pl.krystiankaniowski.composecharts.views
 
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import pl.krystiankaniowski.composecharts.ChartScreen
 import pl.krystiankaniowski.composecharts.autoColor
 import pl.krystiankaniowski.composecharts.column.ColumnChart
 import pl.krystiankaniowski.composecharts.column.ColumnChartData
 import pl.krystiankaniowski.composecharts.column.ColumnChartStyle
+import pl.krystiankaniowski.composecharts.components.*
+import pl.krystiankaniowski.composecharts.utils.generateList
+import pl.krystiankaniowski.composecharts.utils.randomize
+import kotlin.random.Random
 
-@Suppress("MagicNumber")
 @Composable
-fun ColumnChartDemoGrouped() {
-    ColumnChart(
-        data = ColumnChartData(
-            labels = listOf(
-                "C1", "C2", "C3", "C4", "C5",
-            ),
-            columns = listOf(
-                ColumnChartData.Column(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
-                ColumnChartData.Column(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
-                ColumnChartData.Column(label = "Series C", color = autoColor(2), values = listOf(0f, 1f, 2f, 1f, 0f)),
-            )
-        ),
+fun ColumnGroupedChartDemo() {
+    ColumnRawChartDemo(
+        title = "Column Grouped Chart",
         style = ColumnChartStyle.GROUPED,
-        title = { Text("Column chart grouped") },
     )
 }
 
-@Suppress("MagicNumber")
 @Composable
-fun ColumnChartDemoStacked() {
-    ColumnChart(
-        data = ColumnChartData(
-            labels = listOf(
-                "C1", "C2", "C3", "C4", "C5",
-            ),
-            columns = listOf(
-                ColumnChartData.Column(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
-                ColumnChartData.Column(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
-                ColumnChartData.Column(label = "Series C", color = autoColor(2), values = listOf(0f, 1f, 2f, 1f, 0f)),
-            )
-        ),
+fun ColumnStackedChartDemo() {
+    ColumnRawChartDemo(
+        title = "Column Stacked Chart",
         style = ColumnChartStyle.STACKED,
-        title = { Text("Column chart stacked") },
     )
 }
 
-@Suppress("MagicNumber")
 @Composable
-fun ColumnChartDemoProportional() {
-    ColumnChart(
-        data = ColumnChartData(
-            labels = listOf(
-                "C1", "C2", "C3", "C4", "C5",
-            ),
-            columns = listOf(
-                ColumnChartData.Column(label = "Series A", color = autoColor(0), values = listOf(5f, 4f, 3f, 2f, 1f)),
-                ColumnChartData.Column(label = "Series B", color = autoColor(1), values = listOf(1f, 1f, 1f, 1f, 1f)),
-                ColumnChartData.Column(label = "Series C", color = autoColor(2), values = listOf(0f, 1f, 2f, 1f, 0f)),
-            )
-        ),
+fun ColumnProportionalChartDemo() {
+    ColumnRawChartDemo(
+        title = "Column Proportional Chart",
         style = ColumnChartStyle.PROPORTIONAL,
-        title = { Text("Column chart proportional") },
+    )
+}
+
+@Suppress("MagicNumber", "LongMethod")
+@Composable
+private fun ColumnRawChartDemo(title: String, style: ColumnChartStyle) {
+
+    var data by remember {
+        val random = Random(System.currentTimeMillis())
+        val size = 5
+        mutableStateOf(
+            ColumnChartData(
+                labels = buildList {
+                    for (i in 0 until size) {
+                        add("C$i")
+                    }
+                },
+                columns = listOf(
+                    createEntry(random, 0, size),
+                    createEntry(random, 1, size),
+                    createEntry(random, 2, size),
+                ),
+            )
+        )
+    }
+
+    ChartScreen(
+        chart = {
+            ColumnChart(
+                data = data,
+                title = { Text(title) },
+                style = style,
+            )
+        },
+        settings = {
+            OptionRandomize { random ->
+                data = data.copy(
+                    columns = data.columns.map {
+                        it.copy(values = it.values.randomize(random))
+                    },
+                )
+            }
+            OptionAddDataSet { random ->
+                val newId = data.columns.size
+                data = data.copy(columns = data.columns + createEntry(random = random, id = newId, size = data.columns.first().values.size))
+            }
+            OptionRemoveDataSet {
+                data = data.copy(columns = data.columns.dropLast(1))
+            }
+            OptionAddData { random ->
+                data = data.copy(
+                    labels = data.labels + "C${data.labels.size}",
+                    columns = data.columns.map { it.copy(values = it.values + random.nextFloat()) },
+                )
+            }
+            OptionRemoveData {
+                data = data.copy(
+                    labels = data.labels.dropLast(1),
+                    columns = data.columns.map { it.copy(values = it.values.dropLast(1)) },
+                )
+            }
+        }
+    )
+}
+
+private fun createEntry(random: Random, id: Int, size: Int): ColumnChartData.Column {
+    return ColumnChartData.Column(
+        label = "Series ${id + 1}",
+        color = autoColor(id),
+        values = generateList(random, size),
     )
 }
