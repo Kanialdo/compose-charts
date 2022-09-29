1e56f144f53d8ad41ea4e5c0bf67954a301f5c7a
Krystian Kaniowski <kanialdo@gmail.com>
2022-09-29 22:09:55 +0200

    resize desktop window

--- demo-desktop/src/main/kotlin/Main.kt
+++ demo-desktop/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import pl.krystiankaniowski.composecharts.DemoApplication
 import pl.krystiankaniowski.composecharts.DemoTheme
@@ -9,6 +11,10 @@ fun main() = application {
     val icon = painterResource("icon.png")
 
     Window(
+        state = WindowState(
+            width = 1024.dp,
+            height = 768.dp,
+        ),
         onCloseRequest = ::exitApplication,
         icon = icon,
         title = "Compose charts",
