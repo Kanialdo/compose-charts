8ec8f366dd81fb3c15bfd8dab6cd09687bc95dde
Krystian Kaniowski <kaniowski@smartpatient.eu>
2024-02-07 11:02:19 +0100

    detekt fixes

--- demo-web/build.gradle.kts
+++ demo-web/build.gradle.kts
@@ -1,7 +1,6 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 
-        import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-
-        plugins {
+plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose)
 }
@@ -37,4 +36,3 @@ kotlin {
 compose.experimental {
     web.application {}
 }
-
