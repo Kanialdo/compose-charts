a185b0d0495526377e51fb9e7d5066f5694309d3
Krystian Kaniowski <kanialdo@gmail.com>
2024-05-22 21:51:56 +0200

    K2

--- demo-web/build.gradle.kts
+++ demo-web/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
